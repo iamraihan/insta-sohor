c128977437d3ee0ac04627d95dfbb0c331407ab9
iamraihan <trueraihan@gmail.com>
2022-03-10 22:35:23 +0600

    console.log remove

--- js/app.js
+++ js/app.js
@@ -51,7 +51,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post.comments[0].user)
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
